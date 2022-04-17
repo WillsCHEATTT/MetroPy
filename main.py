d6329209433dac29b803627ff0520b47cd6c519a
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-04-17 13:46:50 -0400

    Fix copyright date

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #             MetroPY ("Weather" you like this script or not)             #
 #                                                                         #
 #                                                                         #
-#  Copyright (c) 2020, Zackery .R. Smith <zackery.smith@hsdgreenbush.org>.#
+#  Copyright (c) 2022, Zackery .R. Smith <zackery.smith@hsdgreenbush.org>.#
 #                                                                         #
 #  This program is free software: you can redistribute it and/or modify   #
 #  it under the terms of the GNU General Public License as published by   #
